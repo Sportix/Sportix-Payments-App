35333283440d727b207bfb26b07e2dea7ec2a602
Brad Madigan <bradmadigan@gmail.com>
2017-04-02 16:01:46 -0400

    removed nullable from migratino

--- database/migrations/2017_03_04_201019_create_orders_table.php
+++ database/migrations/2017_03_04_201019_create_orders_table.php
@@ -16,7 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             // $table->unsignedInteger('account_id')->unsigned()->index();
-            $table->string('transaction_id', 64)->nullable();
+            $table->string('transaction_id', 64);
             $table->unsignedInteger('product_id')->unsigned()->index();
             $table->string('product_type')->default('FUND');
             $table->string('email', 100)->index();
@@ -24,6 +24,7 @@ class CreateOrdersTable extends Migration
             $table->integer('payment_amount')->default(0);
             $table->integer('app_fee_percent')->default(0);
             $table->boolean('charge_app_fee')->default(false);
+            $table->string('card_last_four', 16)->nullable();
             $table->timestampsTz();
             $table->softDeletes();
         });
